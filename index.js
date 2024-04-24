20015f6c2df110b256f75ffb0bae442e648d7e38
Lisanne van Vliet <lisannevanvliet@mail.com>
2024-04-24 18:27:23 +0200

    Add other pages

--- index.js
+++ index.js
@@ -10,23 +10,40 @@ app.set('view engine', 'handlebars')
 app.set('views', './views')
 app.use(express.static('public'))
 
-app.get('/', (_req, res) => {
-    const path = 'public/images'
+function sortImages(directory) {
     const images = new Array()
-    const names = []
+    const sortedImages = []
 
-    fs.readdirSync(path).forEach((name, index) => {
-        if (name != '.DS_Store') {
+    fs.readdirSync(directory).forEach((file, index) => {
+        const path = directory + '/' + file
+    
+        if (fs.lstatSync(path).isFile() && file != '.DS_Store') {
             images[index] = {}
-            images[index]['name'] = name
-            images[index]['timestamp'] = ExifReader.load(fs.readFileSync(path + '/' + name))['DateTimeOriginal'].value
+            images[index]['name'] = file
+            images[index]['timestamp'] = ExifReader.load(fs.readFileSync(path))['DateTimeOriginal'].value
         }
     })
 
     images.sort((x, y) => x.timestamp - y.timestamp)
-    images.forEach(image => names.push(image['name']))
-    
-    res.render('home', { images: names })    
+    images.forEach(image => sortedImages.push(image['name']))
+
+    return sortedImages
+}
+
+app.get('/photo-series', (_req, res) => {
+    res.render('photo-series', { images: sortImages('public/images/selfless-activism') })
+})
+
+app.get('/about', (_req, res) => {
+    res.render('about')
+})
+
+app.get('/contact', (_req, res) => {
+    res.render('contact')
+})
+
+app.get('/', (_req, res) => {
+    res.render('home', { images: sortImages('public/images') })
 })
 
 app.listen(3000)
--- public/js/script.js
+++ public/js/script.js
@@ -1,19 +1,29 @@
-const img = document.querySelector('div.overlay div img')
-const overlay = document.querySelector('div.overlay')
-
-document.querySelectorAll('div.images img').forEach(image =>
-    image.addEventListener('click', element => {
-        img.src = element.target.src
-        overlay.classList.add('show')
-        overlay.classList.remove('hide')
-    })
-)
-
-overlay.addEventListener('click', () => {
-    overlay.classList.add('hide')
-    overlay.classList.remove('show')
+document.querySelectorAll('ul li a').forEach(anchor => {
+    if (anchor.textContent == 'Home' && window.location.pathname.substring(1) == '' || anchor.textContent.replace(/\s+/g, '-').toLowerCase() == window.location.pathname.substring(1)) {
+        anchor.classList.add('active')
+    } else {
+        anchor.classList.remove('active')
+    }
 })
 
-img.addEventListener('click', event =>
-    event.stopPropagation()
-)
+if (window.location.pathname.substring(1) == '' || window.location.pathname.substring(1) == 'photo-series') {
+    const overlayImage = document.querySelector('div.overlay div img')
+    const overlay = document.querySelector('div.overlay')
+
+    document.querySelectorAll('div.images img').forEach(image =>
+        image.addEventListener('click', element => {
+            overlayImage.src = element.target.src
+            overlay.classList.add('show')
+            overlay.classList.remove('hide')
+        })
+    )
+    
+    overlay.addEventListener('click', () => {
+        overlay.classList.add('hide')
+        overlay.classList.remove('show')
+    })
+    
+    overlayImage.addEventListener('click', event =>
+        event.stopPropagation()
+    )
+}
